de461fb29a0986abb3e35cb97da62ae22b0d399f
pyounani <yndbsk9372@gmail.com>
2024-04-05 13:41:07 +0900

    [feat] DB 설정 및 엔티티 생성

---
+++ src/main/java/com/example/springJWT/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.springJWT.repository;
+
+import com.example.springJWT.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+
+}
